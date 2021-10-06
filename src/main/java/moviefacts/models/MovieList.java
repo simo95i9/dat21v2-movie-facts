3dd09898d57287593e5382e3c5399042e18c04d7
Simon Gredal <simon.gredal@icloud.com>
2021-10-06 11:11:47 +0200

    Read data from relative path instead

--- src/main/java/moviefacts/models/MovieList.java
+++ src/main/java/moviefacts/models/MovieList.java
@@ -1,5 +1,6 @@
 package moviefacts.models;
 
+import java.io.File;
 import java.io.FileNotFoundException;
 import java.nio.file.Path;
 import java.time.Year;
@@ -9,14 +10,14 @@ import java.util.function.Predicate;
 
 public class MovieList {
     private final List<Movie> movies;
-    private final Path movieDataPath = Path.of("/Users/simon/Downloads/movies.tsv");
+    private final File movieDataFile = new File("src/main/resources/movies.tsv");
 
     public MovieList() {
         this.movies = new ArrayList<>();
     }
 
     public void loadData() {
-        try (Scanner scanner = new Scanner( movieDataPath.toFile() ) ) {
+        try (Scanner scanner = new Scanner( movieDataFile ) ) {
             scanner.nextLine();
             while ( scanner.hasNextLine() ) {
                 String line = scanner.nextLine();
